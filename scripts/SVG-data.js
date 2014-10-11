86d5c03bad48c9c6285eb7865198a89a12c3ef19
peter <peter.collingridge@gmail.com>
2014-10-11 22:35:41 +0100

    Add array of position attributes to data file

--- scripts/SVG-data.js
+++ scripts/SVG-data.js
@@ -11,26 +11,29 @@ var defaultStyles = {
     "opacity": "1",
     "enable-background": "accumulate",
     "fill": "#000",
-    "fill-opacity": "1",
+    "fill-opacity": 1,
     "fill-rule": "nonzero",
     "marker": "none",
     "marker-start": "none",
     "marker-mid": "none",
     "marker-end": "none",
     "stroke": "none",
-    "stroke-width": "1",
-    "stroke-opacity": "1",
-    "stroke-miterlimit": "4",
+    "stroke-width": 1,
+    "stroke-opacity": 1,
+    "stroke-miterlimit": 4,
     "stroke-linecap": "butt",
     "stroke-linejoin": "miter",
     "stroke-dasharray": "none",
-    "stroke-dashoffset": "0",
+    "stroke-dashoffset": 0,
     "stop-opacity": 1,
     "font-anchor": "start",
     "font-style": "normal",
     "font-weight": "normal",
     "font-stretch": "normal",
     "font-variant": "normal",
+    "text-anchor": "start",
+    "text-anchor": "start",
+    "writing-mode": "lr-tb",
     "pointer-events": "visiblePainted"
 };
 
@@ -46,6 +49,9 @@ var nonEssentialStyles = {
 };
 
 // Attribute which determine the size or position of elements
+// The default value of these is 0
 var positionAttributes = [
-
+    "x", "y", "width", "height", "rx", "ry",
+    "cx", "cy", "r",
+    "x1", "x2", "y1", "y2"
 ];
