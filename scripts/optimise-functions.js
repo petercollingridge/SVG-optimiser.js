ecfb1bd07f450e310d5018af9f38926a19fe4151
Peter Collingridge <peter.collingridge@gmail.com>
2016-02-14 13:15:30 +0000

    Add parseNumber and parseStyle functions

--- scripts/optimise-functions.js
+++ scripts/optimise-functions.js
@@ -1,7 +1,25 @@
 var SVG_optimise = {
-    // Take the value for the "d" attribute of a path and return an array of arrays
-    // The first value in each sub-array is a letter refering to the type of path.
-    // The other values are the numerical parameters for that path
+    // Parse digit string to digit, keeping any final units
+    // e.g. "3.4 ex" -> [3.4, 'ex']
+    parseNumber: function(str) {
+        // TODO: Maybe move regex somewhere else
+        var reDigit = /^\s*([-+]?[\d\.]+)(e[-+]?[\d\.]+)?\s*(%|em|ex|px|pt|pc|cm|mm|in)?\s*$/i;
+        var digits = reDigit.exec(str);
+
+        if (digits === null) { return str; }
+
+        var num = { number: parseFloat(digits[1] + (digits[2] || "")) };
+        if (digits[3]) { num.units = digits[3]; }
+
+        return num;
+    },
+
+    /**
+    * Parse the value for the "d" attribute of a path, returning an array of arrays.
+    * The first value in each sub-array is a letter refering to the type of line segment.
+    * The other values are the numerical parameters for that segment.
+    * e.g "M10,20 L23,45" -> [['M', 10, 20], ['L', 23, 45]]
+    */
     parsePath: function(dAttr) {
         var reCommands = /([ACHLMQSTVZ])([-\+\d\.\s,e]*)/gi;
         var reDigits = /([-+]?[\d\.]+)([eE][-+]?[\d\.]+)?/g;
@@ -26,7 +44,23 @@ var SVG_optimise = {
         }
 
         return path;
+    },
+
+    // Split a string from a style attribute into a object of styles
+    // e.g. style="fill:#269276;opacity:1" => {fill: '#269276', opacity: '1'}
+    parseStyle: function(styleString) {
+        var styles = {};
+        var styleArray = styleString.split(/\s*;\s*/);
+        var reColonSplit = /\s*:\s*/;
+        
+        for (var i = 0; i < styleArray.length; i++) {
+            var value = styleArray[i].split(reColonSplit);
+            
+            if (value.length === 2 && value[0] !== "" && value[1] !== "") {
+                styles[value[0]] = this.parseNumber(value[1]);
+            }
+        }
+        
+        return styles;
     }
 };
-
-console.log(SVG_optimise.parsePath("M 270.15082,287.7259 C 275.16461,280.01462 281.37546,272.55352 289.55131,268.13521 C 299.42699,265.07059 309.33566,274.29614 309.8605,283.98778 C 312.05557,294.2993 300.09304,297.48274 292.27448,297.54928 C 281.67864,299.8587 272.51806,305.87794 263.64823,311.78348 C 255.12304,316.49246 245.06588,319.11201 238.24894,326.3488 C 231.95348,331.90474 225.09851,325.23928 229.08306,318.4931 C 232.55509,301.367 240.73269,285.3893 251.96037,272.06476 C 260.56945,259.30823 272.06848,248.66622 280.09597,235.53851 C 283.13341,231.37092 284.62352,224.4998 278.13284,224.47057 C 273.00556,216.03772 280.23289,209.7938 279.3532,197.91107 C 276.22736,189.50426 274.53198,183.38562 268.70683,178.80434 C 260.21687,169.7795 252.26797,166.79306 237.24626,159.16725 C 218.26467,155.4031 211.74964,154.26706 231.76887,163.38204 C 233.18419,172.3556 217.02149,165.11534 213.05423,171.08331 C 205.11218,174.66054 223.92868,172.66761 219.92099,178.73114 C 215.61855,177.65843 196.73394,191.8475 211.53082,186.78126 C 218.67969,189.14659 224.29481,194.91609 231.25932,197.89285 C 238.1055,200.68645 245.6366,202.06576 251.47445,206.97424 C 259.0014,212.6582 266.12563,218.89427 274.20199,223.80491 C 275.48884,224.80193 276.71285,225.88642 277.80296,227.09755"));
--- tests/qunit-tests.js
+++ tests/qunit-tests.js
@@ -1,4 +1,5 @@
 QUnit.test("parsePath", function(assert) {
+  assert.deepEqual(SVG_optimise.parsePath(""), [], "Empty string");
   assert.deepEqual(SVG_optimise.parsePath("M 10 23 L 45 789"), [['M', 10, 23], ['L', 45, 789]], "Integers and spaces");
   assert.deepEqual(SVG_optimise.parsePath("M 10 23 a150,254 0 0 0 15,150"), [['M', 10, 23], ['a', 150, 254, 0, 0, 0, 15, 150]], "Multiple values");
   assert.deepEqual(SVG_optimise.parsePath("M 10 23 a 150 254 0 0 0 15,150z"), [['M', 10, 23], ['a', 150, 254, 0, 0, 0, 15, 150], ['z']], "Final z");
@@ -7,4 +8,35 @@ QUnit.test("parsePath", function(assert) {
   assert.deepEqual(SVG_optimise.parsePath("M 10.0 23.2 L 45.0001 0.00"), [['M', 10, 23.2], ['L', 45.0001, 0]], "Decimals");
   assert.deepEqual(SVG_optimise.parsePath("M 10.0-23.2 L-45.1-0.00, -4 3"), [['M', 10, -23.2], ['L', -45.1, 0, -4, 3]], "Negatives");
   assert.deepEqual(SVG_optimise.parsePath("M 12.3 2.3e2 L-1e1 -7e-3"), [['M', 12.3, 230], ['L', -10, -0.007]], "Exponents");
+});
+
+QUnit.test("parseNumber", function(assert) {
+  assert.deepEqual(SVG_optimise.parseNumber(""), "", "Empty string");
+  assert.deepEqual(SVG_optimise.parseNumber("0"), { number: 0 }, "Zero");
+  assert.deepEqual(SVG_optimise.parseNumber("0.000"), { number: 0 }, "Zero point Zero");
+  assert.deepEqual(SVG_optimise.parseNumber("12"), { number: 12 }, "Integer");
+  assert.deepEqual(SVG_optimise.parseNumber("1.0230"), { number: 1.023 }, "Decimal");
+  assert.deepEqual(SVG_optimise.parseNumber("-1.0230"), { number: -1.023 }, "Negative");
+  assert.deepEqual(SVG_optimise.parseNumber("+1.0230"), { number: 1.023 }, "Leading plus");
+  assert.deepEqual(SVG_optimise.parseNumber("1.023e4"), { number: 10230 }, "Exponent");
+  assert.deepEqual(SVG_optimise.parseNumber("1.023e+4"), { number: 10230 }, "Exponent with plus");
+  assert.deepEqual(SVG_optimise.parseNumber("-10.2E-2"), { number: -0.102 }, "Negative exponent");
+  assert.deepEqual(SVG_optimise.parseNumber("-1.2%"), { number: -1.2, units: '%' }, "Percent");
+  assert.deepEqual(SVG_optimise.parseNumber("-1.23e3 px"), { number: -1230, units: 'px' }, "Pixels");
+  assert.deepEqual(SVG_optimise.parseNumber("-1.23e3EM"), { number: -1230, units: 'EM' }, "Em");
+  assert.deepEqual(SVG_optimise.parseNumber("1-2"), '1-2', "Subtract");
+  assert.deepEqual(SVG_optimise.parseNumber("1e"), '1e', "Leading number");
+  assert.deepEqual(SVG_optimise.parseNumber("word1"), 'word1', "Trailing number");
+  assert.deepEqual(SVG_optimise.parseNumber("#2692e6"), '#2692e6', "Hex colour");
+});
+
+QUnit.test("parseStyle", function(assert) {
+  assert.deepEqual(SVG_optimise.parseStyle(""), {}, "Empty string");
+  assert.deepEqual(SVG_optimise.parseStyle("fill"), {}, "String");
+  assert.deepEqual(SVG_optimise.parseStyle("fill:"), {}, "Missing value");
+  assert.deepEqual(SVG_optimise.parseStyle(":red"), {}, "Missing key");
+  assert.deepEqual(SVG_optimise.parseStyle("fill:#269276"), { fill: '#269276' }, "Non-number");
+  assert.deepEqual(SVG_optimise.parseStyle("fill:#269276;opacity:1"), { fill: '#269276', opacity: { number: 1 } }, "Two values");
+  assert.deepEqual(SVG_optimise.parseStyle("fill: #269276; opacity :1;"), { fill: '#269276', opacity: { number: 1 } }, "Trailing semi-colon");
+  assert.deepEqual(SVG_optimise.parseStyle("font-size: 12px ; opacity:0.9%"), { 'font-size': { number: 12, units: 'px' }, opacity: { number: 0.9, units: '%' }}, "With units");
 });
